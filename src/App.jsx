913abb422e3d736f0e1efb3f314d2569e447fc62
Kushian01100111 <comaspedro6@gmail.com>
2023-07-06 18:12:59 -0300

    06/07

--- src/App.jsx
+++ src/App.jsx
@@ -33,27 +33,29 @@ useEffect(() => {
           whileInView="whileInView"
           viewport={{ once: true }}>
               <div className='area'>
-                <h1>Hace Crecer con tus ventas</h1>
+                <h1 className='h1'>Hace Crecer tus ventas</h1>
                 <m.hr animate={
                   width <= 370 ? {x: 82, y: -16}
                   : width <= 500 ? {x: 128, y: -22}
-                  : width <= 960 ? {x: 128, y: -31}  
-                  : width <= 1090 ? {x: -54, y: -31}
-                 : width <= 1200 ? {x: -45, y: -34}
-                  : {x: 95, y: -44}}/>
+                  : width <= 960 ? {x: -199, y: 19}  
+                  : width <= 1090 ? {x: -256, y: 21}
+                 : width <= 1200 ? {x: -313, y: 19}
+                  : {x: -337, y: 12}}/>
                 <m.h1 animate={
                   width <= 370 ? {x: 141, y: -30}
-                  : width <= 500 ? {x: 201, y: -37} 
-                  : width <= 1090 ? {x: 250, y: -55}
-                 : width <= 1200 ? {x: 251, y: -61}
-                  : {x: 394, y: -77}}  
+                  : width <= 500 ? {x: 201, y: -37}
+                  : width <= 960 ? {x: -80, y: -4}  
+                  : width <= 1090 ? {x: 50, y: -4}
+                 : width <= 1200 ? {x: 0, y: -6}
+                  : {x: -48, y: -17}}  
                 className='parti2'> en el </m.h1>
                 <m.h1 animate={
                   width <= 370 ? {x: 77, y: -28}
                   : width <= 500 ? {x: 94, y: -31}
-                  : width <= 1090 ? {x: 42, y: -50}
-                 : width <= 1200 ? {x: 59, y: -60}
-                  : {x: 97, y: -82} } 
+                  : width <= 960 ? {x: 174, y: -53} 
+                  : width <= 1090 ? {x: 194, y: -51}
+                 : width <= 1200 ? {x: 153, y: -60}
+                  : {x: 135, y: -80} } 
                 className='parti2 parti3'>mundo digital.</m.h1>
               </div>
               <p>Queremos acompañarte a que tu empresa se expanda y que sean cada vez las personas que conozcan tus productos y servicios</p>
